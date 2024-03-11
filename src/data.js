fc69cda28e8a7fec5a4240c7bb1e9998abdf8b0a
Krish Bajaj <53073541+Krish-Bajaj@users.noreply.github.com>
2024-03-11 19:56:03 +0530

    chore: addition of testimonials

--- src/data.js
+++ src/data.js
@@ -103,37 +103,65 @@ export const commitmentsData = [
 export const MLPtestimonials = [
 	{
 		image: client,
-		name: 'Jacob Joshua',
-		designation: 'Chief Manager',
+		name: 'Devanshi',
+		designation: 'Founder, Label Devanshi Didwania',
 		testimonial:
-			"I found the digital expertise I needed. Their creative-professional balance exceeded expectations. Friendly interactions, exceptional outcomes. For digital enchantment, it's got to be Embrace!",
+			"Ive been working with Mahikah for the last month and l'd like to say that it's been an absolute pleasure! Not only is she super hard working but super helpful with anything and everything. She goes out of her way to make sure the work is done and fully understands her clients requirements which leads to a perfect harmony of interests! Lastly I'd like to add that you can totally trust her with your work and know that she'll deliver on time with exactly what you envision!",
 	},
 	{
 		image: client,
-		name: 'Jacob Joshua',
-		designation: 'Chief Manager',
+		name: 'Akshay Nihalani',
+		designation: 'Co-founder, Sagar Nihalani',
 		testimonial:
-			"I found the digital expertise I needed. Their creative-professional balance exceeded expectations. Friendly interactions, exceptional outcomes. For digital enchantment, it's got to be Embrace!",
+			"We've had the pleasure of collaborating with Mahikah and Socialcapitalist101 for the past two years, and it's been an exceptional partnership. The social media management team consistently delivers creative, strategic, and impactful campaigns that have significantly elevated our online presence. Their attention to detail, responsiveness, and innovative approach make them an invaluable asset to our brand. Looking forward to many more successful collaborations!",
 	},
 	{
 		image: client,
-		name: 'Jacob Joshua',
-		designation: 'Chief Manager',
+		name: 'Deepika',
+		designation: 'Founder, Label Kyrabyninadeepika',
 		testimonial:
-			"I found the digital expertise I needed. Their creative-professional balance exceeded expectations. Friendly interactions, exceptional outcomes. For digital enchantment, it's got to be Embrace!",
+			"It has been an absolute delight working with the Social Capitalist. This brilliant team epitomises passion, dedication and taste like nobody else. The power to translate client's needs to perfect deliverables is definitely Paridhi's and her team's forte.",
 	},
 	{
 		image: client,
-		name: 'Jacob Joshua',
-		designation: 'Chief Manager',
+		name: 'Tracy',
+		designation: 'Founder, The Naviya Edit',
 		testimonial:
-			"I found the digital expertise I needed. Their creative-professional balance exceeded expectations. Friendly interactions, exceptional outcomes. For digital enchantment, it's got to be Embrace!",
+			"Working with Mahikah & team has been incredible. We have worked together almost a year & have created amazing social media presence. I respect how in sync the team are with ideation, keeping on trends, curating informative & fun stories all whilst sticking with the brand's values. I've learnt a lot from the team & love how responsive they are despite being a UK fashion brand. I would highly recommend them to any small business looking to really elevate and focus on their social media or simply need support in this field.",
 	},
 	{
 		image: client,
-		name: 'Jacob Joshua',
-		designation: 'Chief Manager',
+		name: 'Harsh Bhandari',
+		designation: 'Co-Founder, Bhandari Jewels',
 		testimonial:
-			"I found the digital expertise I needed. Their creative-professional balance exceeded expectations. Friendly interactions, exceptional outcomes. For digital enchantment, it's got to be Embrace!",
+			"Social Capitalist is one the best Digital Marketing Team right now in Kolkata. The Team members are very hardworking and put a lot effort! They are very efficient to work with.",
+	},
+	{
+		image: client,
+		name: 'Drishti Nihalani',
+		designation: 'Founder, Purple Boutique',
+		testimonial:
+			"Working with Socialcapitalist101 has been a game-changer for our brand's online presence. The social media management team's dedication, creativity, and meticulous planning have not only increased our engagement but also fostered a genuine connection with our audience. The fact that they have the quickest response to all our queries and needs makes our work super efficient and organized. Their commitment to excellence shines through, making them an indispensable part of Purple's marketing strategy.",
+	},
+	{
+		image: client,
+		name: 'Nupur Wadhwani',
+		designation: 'Co-Founder, Wow Kids',
+		testimonial:
+			"Social capitalist has been wonderful through the journey so far. My expectations have been genuinely surpassed by their performance and the experience they provide to us. They're always on their toes, so energized and enthusiastic about everything happening on the social media platform which helps our page perform so much better! We valued insights and newest trending suggestions provided by Mahikah which enhanced our strategy right away. They certainly know their job well!",
+	},
+	{
+		image: client,
+		name: 'Himanshu Rohra',
+		designation: 'Director, Hi Bao',
+		testimonial:
+			"Social Capitalist has a very professional and dedicated young team. The ideas, designs and the product they deliver continue to exceed every expectation that we set. Would strongly and surely recommend to anyone who is looking for anything related to Digital Marketing, Social Media Management or Graphic Design. They're the best!",
+	},
+	{
+		image: client,
+		name: 'Vineet Gupta',
+		designation: 'Founder, Kamakhya Kolkata',
+		testimonial:
+			"Social capitalist is a team of young and energetic individuals. They did a brilliant job to grow my business digitally and get better leads. Thank you for the efforts.",
 	},
 ]
